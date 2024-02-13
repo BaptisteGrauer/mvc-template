592e1124e17bb1272ce773512863e99001eb029c
Baptiste <baptiste.grosr@gmail.com>
2024-02-13 15:31:34 +1100

    ajout default.php + update Controller.php

--- app/Controller.php
+++ app/Controller.php
@@ -19,6 +19,7 @@ abstract class Controller
 // On crée une instance de ce modèle. Ainsi "Articles" sera accessible par $this->Articles
         $this->$model = new $c_model();
     }
+
     /**
      * Afficher une vue
      *
@@ -26,11 +27,17 @@ abstract class Controller
      * @param array $data
      * @return void
      */
-    public function render(string $fichier, array $data = []){
+    public function render(string $fichier, array $data = [])
+    {
 // Récupère les données et les extrait sous forme de variables
         extract($data);
+// On démarre le buffer de sortie
+        ob_start();
 // Crée le chemin et inclut le fichier de vue
-        require_once(ROOT.'views/'.explode("\\",strtolower(get_class($this))
-            )[1].'/'.$fichier.'.php');
+        require_once(ROOT . 'views/' . explode("\\", strtolower(get_class($this)))[1] . '/' . $fichier . '.php');
+// On stocke le contenu dans $content
+        $content = ob_get_clean();
+// On fabrique le "template"
+        require_once(ROOT . 'views/layout/default.php');
     }
 }
